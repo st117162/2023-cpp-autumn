ce3d1fc736ab6c6cb222e7295b51677460d79f3c
st117162 <144272076+st117162@users.noreply.github.com>
2023-10-11 08:42:08 +0300

    Homework04 Project07 code update

--- 2023.10.09-homework-04/Project07/Source.cpp
+++ 2023.10.09-homework-04/Project07/Source.cpp
@@ -3,19 +3,40 @@
 
 int main(int, char**)
 {
-	int n = 0;
-	std::cin >> n;
-	int* a = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; ++i)
-	{
-		std::cin >> *(a + i);
-	}
-	free(*(a));
-	for (int i = 0; i < n; ++i)
-	{
-		std::cout << *(a + i);
-	}
-	std::cout << std::endl;
-	free(a);
-	return EXIT_SUCCESS;
+  int n = 0;
+  std::cin >> n;
+  
+  int* a = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; ++i)
+  {
+    std::cin >> *(a + i);
+  }
+
+  for (int i = 0; i < n; ++i)
+  {
+    if (*(a + i) <= 0)
+    {
+      for (int j = i; j < n - 1; ++j)
+      {
+        *(a + j) = *(a + j + 1);
+      }
+      --n;
+    }
+  }
+
+  int* b = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; ++i)
+  {
+    *(b + i) = *(a + i);
+  }
+  free(a);
+  a = b;
+
+  for (int i = 0; i < n; ++i)
+  {
+    std::cout << *(a + i) << " ";
+  }
+  std::cout << std::endl;
+  free(a);
+  return EXIT_SUCCESS;
 }
